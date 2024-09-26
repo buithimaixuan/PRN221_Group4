0b6bbffd67a6dfe95f63800606a965c05a7a7a92
thucnhce171748 <thucnhce171748@fpt.edu.vn>
2024-09-26 09:31:36 +0700

    add project

---
+++ Project_PRN221_Group4/Project_PRN221_Group4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_PRN221_Group4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
